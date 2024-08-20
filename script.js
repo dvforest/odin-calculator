0bd3f11b6b1ceec21ce25cf3204e39c35b39c910
dvforest <dvforest@hotmail.com>
2024-08-19 20:10:38 -0400

    Move code to separate html, js and css files.

---
+++ script.js
@@ -0,0 +1,38 @@
+let a, b;
+let op = "";
+let clampDecimals = 8; 
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(op, a, b) {
+    let result = 0;
+    if (op === "+") {
+        result = add(a, b);
+    }
+    if (op === "-") {
+        result = subtract(a, b);
+    }
+    if (op === "*") {
+        result = multiply(a, b);
+    }
+    if (op === "/") {
+        result = divide(a, b);
+    }
+    return Number(result.toFixed(clampDecimals)); // Number() removes unnecessary zeros
+}
+
+console.log(operate("/", 5, 12));
