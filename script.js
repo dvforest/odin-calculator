994c904d638353ae7ee1a93426943575e750c6a0
dvforest <dvforest@hotmail.com>
2024-08-19 20:50:02 -0400

    Add 0-9 buttons

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let a, b;
 let op = "";
-let clampDecimals = 8; 
+const clampDecimals = 8;
+const width = 200;
+const height = 300;
+
+const container = document.querySelector(".flex-container");
+container.style.width = width + "px";
+container.style.height = height + "px";
+
+const orderedNumbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+orderedNumbers.forEach(num => {
+    let button = document.createElement("button");
+    button.textContent = num;
+    button.style.width = width / 3 + "px";
+    button.setAttribute("id", num);
+    button.setAttribute("class", "number");
+    container.appendChild(button);
+});
 
 function add(a, b) {
     return a + b;
