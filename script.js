ed401b93b2fd9fe4605f4eec1056fd7f9ddea09b
dvforest <dvforest@hotmail.com>
2024-08-31 21:11:08 -0400

    Simplify the calculator's display update.
    - Remove operation div tand input to only have one display div.
    - Move the update function outside of the input object.

--- script.js
+++ script.js
@@ -7,43 +7,16 @@ let input = {
     a: 0,
     b: 0,
     operator: "",
-    operation: "",
-    
-    update: function(button) {
-        //if a number is clicked        
-            //if operator is ""
-                //if a is zero
-                    // a = button clicked
-                //else
-                    // a = a + button clicked (as string, then back to number)
-            //else
-                //if b is zero
-                    // b = button clicked
-                //else
-                    //b = b + button clicked (as string, then back to number)
-        //if an operator is clicked
-            //if it's "="
-                // operation = a + " " + operator + " " + b + "="
-                //operate(operator, a, b)            
-            //else
-                //operator = button clicked
-                //operation = a + " " + operator
-    },
 }
 
 const mainDiv = document.querySelector(".main-div");
 mainDiv.style.width = width + "px";
 mainDiv.style.height = height + "px";
 
-const operationDiv = document.createElement("div");
-operationDiv.setAttribute("class", "operation-div");
-operationDiv.textContent = "0";
-mainDiv.appendChild(operationDiv);
-
-const inputDiv = document.createElement("div");
-inputDiv.setAttribute("class", "input-div");
-inputDiv.textContent = "0";
-mainDiv.appendChild(inputDiv);
+const displayDiv = document.createElement("div");
+displayDiv.setAttribute("class", "display-div");
+displayDiv.textContent = "0";
+mainDiv.appendChild(displayDiv);
 
 const topOperatorDiv = document.createElement("div");
 topOperatorDiv.setAttribute("class", "top-operator-div");
@@ -67,7 +40,7 @@ numbers.forEach(num => {
     button.textContent = num;
     button.setAttribute("id", num);
     button.setAttribute("class", "number-button");
-    button.addEventListener("click", (e) => {console.log(`input number ${e.target.id}`)});
+    button.addEventListener("click", (e) => {updateDisplay(e.target.id)});
     numberDiv.appendChild(button);
 });
 
@@ -93,16 +66,16 @@ const operators = [
         div: "right",
     },
 ];
+
 operators.forEach(op => {
     let button = document.createElement("button");
     button.textContent = op.type;
     button.setAttribute("id", op.type);
     button.setAttribute("class", "operator-button");
-    button.addEventListener("click", (e) => {console.log(`input operator ${e.target.id}`)});
+    button.addEventListener("click", (e) => {updateDisplay(e.target.id)});
     let div = document.getElementsByClassName(`${op.div}-operator-div`)[0];
     div.appendChild(button);
 });
-
 
 function add(a, b) {
     return a + b;
@@ -135,4 +108,8 @@ function operate(op, a, b) {
         result = divide(a, b);
     }
     return Number(result.toFixed(clampDecimals)); // Number() removes unnecessary zeros
+}
+
+function updateDisplay(button){
+    displayDiv.textContent += button;
 }
